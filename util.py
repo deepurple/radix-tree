a23103802efdadbeb069f2fd97b642546c17446e
deepurple <junyuanleng@gmail.com>
2016-04-11 01:17:40 +0800

    Add ip address validator

--- util.py
+++ util.py
@@ -10,9 +10,18 @@ def bin_to_ip(bin_str):
                      str(int(bin_str[16:24], 2)),
                      str(int(bin_str[24:32], 2))])
 
+def ip_is_valid(ip_str):
+    return all(map(lambda x: 0 <= x <= 255, [int(ip) for ip in ip_str.split('.')]))
+
+def bin_is_valid(bin_str):
+    ip_str = bin_to_ip(bin_str)
+    return ip_is_valid(ip_str)
+
 if __name__ == '__main__':
     ip_addr = '192.168.0.1'
     bin_str = ip_to_bin(ip_addr)
     ip_str = bin_to_ip(bin_str)
     print bin_str
     print ip_str
+    print ip_is_valid(ip_addr)
+    print bin_is_valid(bin_str)
